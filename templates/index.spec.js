01c1d92ce6a7f97a9dd44c95b651adc1077141bd
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-09-04 15:00:09 -0700

    Add test environment config

--- templates/index.spec.js
+++ templates/index.spec.js
@@ -1,8 +1,12 @@
-import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/svelte";
-import index from "./index.svelte";
+/**
+ * @jest-environment jsdom
+ */
 
-test("shows proper heading when rendered", () => {
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/svelte';
+import index from './index.svelte';
+
+test('shows proper heading when rendered', () => {
   const { getByText } = render(index);
-  expect(getByText("Hello world!")).toBeInTheDocument();
+  expect(getByText('Hello world!')).toBeInTheDocument();
 });
